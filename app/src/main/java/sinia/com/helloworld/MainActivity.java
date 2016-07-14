c1a6092263bc33977f2b14cf05f34795ccb5f8ee
jackvvv <954828748@qq.com>
2016-07-14 11:47:42 +0800

    测试提交

--- app/src/main/java/sinia/com/helloworld/MainActivity.java
+++ app/src/main/java/sinia/com/helloworld/MainActivity.java
@@ -62,7 +62,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
